439d69eade43112b778c67b09a4aa69e9ed63594
hsobczyk <104222030+hsobczyk@users.noreply.github.com>
2022-04-22 13:25:00 -0500

    added type hinting
    added type hinting.

--- classes.py
+++ classes.py
@@ -1,28 +1,28 @@
 class Television:
-    MIN_CHANNEL = 0     # Minimum TV channel
-    MAX_CHANNEL = 3     # Maximum TV channel
+    MIN_CHANNEL: int = 0     # Minimum TV channel
+    MAX_CHANNEL: int = 3     # Maximum TV channel
 
-    MIN_VOLUME = 0      # Minimum TV volume
-    MAX_VOLUME = 2      # Maximum TV volume
+    MIN_VOLUME: int = 0      # Minimum TV volume
+    MAX_VOLUME: int = 2      # Maximum TV volume
 
-    def __init__(self):
+    def __init__(self) -> None:
         """
         Function to initialize the television's properties __channel, __volume, and __status.
         :param self: The object being passed.
         """
-        self.__channel = Television.MIN_CHANNEL   # Set __channel to MIN_CHANNEL by default 
-        self.__volume = Television.MIN_VOLUME     # Set __volume to MIN_VOLUME by default
-        self.__status = False                     # Set __status False by default, "off".
+        self.__channel: int = Television.MIN_CHANNEL   # Set __channel to MIN_CHANNEL by default 
+        self.__volume: int = Television.MIN_VOLUME     # Set __volume to MIN_VOLUME by default
+        self.__status: bool = False                    # Set __status False by default, "off".
 
 
-    def power(self):
+    def power(self) -> None:
         """
         Function to change the property __status of the television alternating between True and False with each run.
         :param self: the object being passed.
         """
         self.__status = not self.__status
 
-    def channel_up(self):
+    def channel_up(self) -> None:
         """
         Function to increment the television's property __channel up by one and to roll over back to MIN_CHANNEL when above MAX_CHANNEL
         :param self: the object being passed.
@@ -32,7 +32,7 @@ class Television:
             if self.__channel > Television.MAX_CHANNEL:
                 self.__channel = Television.MIN_CHANNEL
 
-    def channel_down(self):
+    def channel_down(self) -> None:
         """
         Function to increment the television's property __channel down by one and to roll over back to MAX_CHANNEL when below MIN_CHANNEL
         :param self: the object being passed.
@@ -42,7 +42,7 @@ class Television:
             if self.__channel < Television.MIN_CHANNEL:
                 self.__channel = Television.MAX_CHANNEL
 
-    def volume_up(self):
+    def volume_up(self) -> None:
         """
         Function to increment the television's property __volume up by one and to stop when it reaches MAX_VOLUME
         :param self: the object being passed.
@@ -51,7 +51,7 @@ class Television:
             if self.__volume < Television.MAX_VOLUME:
                 self.__volume += 1
 
-    def volume_down(self):
+    def volume_down(self) -> None:
         """
         Function to increment the television's property __volume down by one ant to stop when it reaches MIN_VOLUME
         :param self: the object being passed.
@@ -60,7 +60,7 @@ class Television:
             if self.__volume > Television.MIN_VOLUME:
                 self.__volume -= 1
 
-    def __str__(self):
+    def __str__(self) -> str:
         """
         Function that modifies what Television returns when its outputed as a string.
         :param self: the object being passed.

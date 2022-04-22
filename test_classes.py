e7e28384435dab419d8b5b05f3793d92146edcb2
hsobczyk <104222030+hsobczyk@users.noreply.github.com>
2022-04-22 14:23:27 -0500

    test volume up
    test for volume_up

--- test_classes.py
+++ test_classes.py
@@ -17,4 +17,16 @@ class Test:
         assert self.tv.__str__() == "TV status: Is on = True, Channel = 0, Volume = 0"
         self.tv.power()
         assert self.tv.__str__() == "TV status: Is on = False, Channel = 0, Volume = 0"
-    
+
+    def test_volume_up(self):
+        self.tv.volume_up()
+        assert self.tv.__str__() == "TV status: Is on = False, Channel = 0, Volume = 0"
+
+        self.tv.power()
+        self.tv.volume_up()
+        assert self.tv.__str__() == "TV status: Is on = True, Channel = 0, Volume = 1"
+
+        self.tv.volume_up()
+        self.tv.volume_up()
+        self.tv.volume_up()
+        assert self.tv.__str__() == "TV status: Is on = True, Channel = 0, Volume = 2"

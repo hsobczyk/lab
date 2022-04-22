3a00110cc9218a9c1098e80506ab742487e6a3b5
hsobczyk <104222030+hsobczyk@users.noreply.github.com>
2022-04-22 14:36:11 -0500

    created test for incrementing channel down
    created test_channel_down

--- test_classes.py
+++ test_classes.py
@@ -58,3 +58,15 @@ class Test:
         self.tv.channel_up()
         self.tv.channel_up()
         assert self.tv.__str__() == "TV status: Is on = True, Channel = 0, Volume = 0"
+
+    def test_channel_down(self):
+        self.tv.channel_down()
+        assert self.tv.__str__() == "TV status: Is on = False, Channel = 0, Volume = 0"
+
+        self.tv.power()
+        self.tv.channel_down()
+        assert self.tv.__str__() == "TV status: Is on = True, Channel = 3, Volume = 0"
+
+        self.tv.channel_down()
+        self.tv.channel_down()
+        assert self.tv.__str__() == "TV status: Is on = True, Channel = 1, Volume = 0"
